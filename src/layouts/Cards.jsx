2335159f10d75b9d2184033de1d2e39547ce533e
TeDi-A <tediadjene@gmail.com>
2025-04-23 23:48:06 +0100

    Update Motion

--- src/layouts/Cards.jsx
+++ src/layouts/Cards.jsx
@@ -2,11 +2,11 @@ import { motion, useTransform, useScroll } from "motion/react";
 
 export default function Cards() {
   const imgs = [
-    "../img-1.jpg",
-    "../img-2.jpg",
-    "../img-3.jpg",
-    "../img-4.jpg",
-    "../img-5.jpg",
+    "../img-7.jpg",
+    "../img-12.jpg",
+    "../img-9.jpg",
+    "../img-14.jpg",
+    "../img-13.jpg",
   ];
 
   const colors = [
