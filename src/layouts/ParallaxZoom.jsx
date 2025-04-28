4a93c7cc66739f121037ed582c37062c22590b8e
TeDi-A <tediadjene@gmail.com>
2025-04-28 14:18:44 +0100

    Update Motion

--- src/layouts/ParallaxZoom.jsx
+++ src/layouts/ParallaxZoom.jsx
@@ -8,30 +8,48 @@ export default function ParallaxZoom() {
     offset: ["start start", "end end"],
   });
   const imgs = [
-    "../img-16.jpg",
-    "../img-12.jpg",
     "../img-13.jpg",
     "../img-14.jpg",
-    "../img-15.jpg",
+    "../img-12.jpg",
+    "../img-16.jpg",
     "../img-17.jpg",
     "../img-18.jpg",
     "../img-19.jpg",
+    "../img-15.jpg",
   ];
 
   const scaleImgA = useTransform(scrollYProgress, [0, 1], [1, 4]);
-  const scaleImgB = useTransform(scrollYProgress, [0, 1], [1, 5]);
-  const scaleImgC = useTransform(scrollYProgress, [0, 1], [1, 6]);
+
+  const itemWidth = window.innerWidth * 0.25;
+  const itemHeight = window.innerHeight * -0.7;
+  const itemRight = window.innerWidth * -0.15;
+
+  const WallX = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, window.innerWidth * -1 - itemWidth - itemRight]
+  );
+  const WallY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, window.innerHeight * -1 - itemHeight]
+  );
+  const scaleImgB = useTransform(scrollYProgress, [0, 1], [1, 1.25]);
+  const scaleImgC = useTransform(scrollYProgress, [0, 1], [1, 1.5]);
   return (
     <>
-      <div className=" justify-center pt-[100vh] w-screen h-[400vh] bg-zinc-950 text-white ">
+      <div className=" justify-center pt-[100vh] w-screen h-[400vh] bg-zinc-900 text-white ">
         <div
           ref={imgRef}
-          className="sticky-container h-[200vh] relative bg-zinc-900"
+          className="sticky-container h-[200vh] w-screen relative bg-black overflow-hidden"
         >
-          <motion.div className="sticky-content h-screen sticky top-0 overflow-hidden">
+          <motion.div
+            className="sticky-content h-screen w-screen sticky top-0  "
+            style={{ scale: scaleImgA, translateX: WallX, translateY: WallY }}
+          >
             <motion.div
-              className="image-container top-0 absolute w-full h-full "
-              style={{ scale: scaleImgA }}
+              className="image-container top-0 absolute w-full h-full border-0"
+              style={{ scale: scaleImgB }}
             >
               <motion.div
                 className="center-img w-[25vw] h-[25vh] relative left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-center bg-cover"
@@ -44,7 +62,7 @@ export default function ParallaxZoom() {
               style={{ scale: scaleImgB }}
             >
               <motion.div
-                className="surround-img w-[25vw] h-[65vh] absolute right-0 top-1/3 -translate-x-1/3 -translate-y-1/2 bg-center bg-cover"
+                className="surround-img w-[25vw] h-[60vh] absolute right-0 top-1/3 -translate-x-1/3 -translate-y-1/2 bg-center bg-cover"
                 style={{ backgroundImage: `url(${imgs[1]})` }}
               ></motion.div>
             </motion.div>
@@ -59,12 +77,9 @@ export default function ParallaxZoom() {
               ></motion.div>
             </motion.div>
 
-            <motion.div
-              className="image-container top-0 absolute w-full h-full "
-              style={{ scale: scaleImgB }}
-            >
+            <motion.div className="image-container top-0 absolute w-full h-full ">
               <motion.div
-                className="surround-img w-[25vw] h-[30vh] absolute right-0 bottom-0 -translate-x-1/2 translate-y-0 bg-center bg-cover"
+                className="star-img w-[25vw] h-[30vh] absolute right-[10vw] bottom-0 translate-y-0 bg-center bg-cover"
                 style={{ backgroundImage: `url(${imgs[3]})` }}
               ></motion.div>
             </motion.div>
